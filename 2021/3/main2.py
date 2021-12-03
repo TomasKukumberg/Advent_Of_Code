15e00820db171a582489290d4497571bedc06fc0
TomasKukumberg <tomas.kukumberg@centrum.sk>
2021-12-03 23:24:36 +0100

    2021-3-second

--- 2021/3/main2.py
+++ 2021/3/main2.py
@@ -1,20 +1,17 @@
-def getMostCommonBitInCol(arr, idx):
+def getCommonBitInCol(arr, idx, arg):
     zeroes = ones = 0
+    
     for el in arr:
         if el[idx] == '0':
             zeroes += 1
         else:
             ones += 1
-    return '0' if zeroes > ones else '1'
+    
+    if arg == 'most':
+        return '1' if zeroes > ones else '0'
+    else:
+        return '0' if zeroes > ones else '1' 
 
-def getLeastCommonBitInCol(arr, idx):
-    zeroes = ones = 0
-    for el in arr:
-        if el[idx] == '0':
-            zeroes += 1
-        else:
-            ones += 1
-    return '1' if zeroes > ones else '0'
 
 def removeAllInvalidRows(arr, most, idx):
     arr2 = arr.copy()
@@ -24,25 +21,16 @@ def removeAllInvalidRows(arr, most, idx):
     return arr2
 
 
-
-
-"""
-determine most common col number, then remove all rows without this number. repeat until last element remains
-"""
-
 with open("input.txt") as f:
     lines = f.read().splitlines()
     lines2 = lines.copy()
 
     for i in range(0, 12):
-        most = getMostCommonBitInCol(lines, i)
+        most = getCommonBitInCol(lines, i, 'most')
         lines = removeAllInvalidRows(lines, most, i)
 
-    print(lines)
-
     for i in range(0, 12):
-        most = getLeastCommonBitInCol(lines2, i)
-        lines2 = removeAllInvalidRows(lines2, most, i)
+        least = getCommonBitInCol(lines2, i, 'least')
+        lines2 = removeAllInvalidRows(lines2, least, i)
     
-    print(int(lines[0], 2) * int(lines2[0], 2))
-    #print(lines2)
+    print(f'Life support rating of the submarine is {int(lines[0], 2) * int(lines2[0], 2)}')
