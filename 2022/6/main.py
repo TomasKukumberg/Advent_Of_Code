76ef94b6721842e35ec8f1343875c1d171118e35
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2022-12-06 12:08:16 +0100

    aoc-2022-6

---
+++ 2022/6/main.py
@@ -0,0 +1,18 @@
+def get_coms(path):
+    with open(path) as f:
+        tmp = "".join(f.read().splitlines())
+        return [*tmp]
+
+def is_marker(arr):
+    return len(arr) == len(set(arr))
+
+coms = get_coms('input.txt')
+marker_index = None
+window_size = 4
+
+for i in range(len(coms)):
+    if is_marker(coms[i:i+window_size]):
+        marker_index = i + window_size
+        break
+    
+print(marker_index)
