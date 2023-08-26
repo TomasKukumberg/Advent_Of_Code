5a6838f2a24c8f616b3733dd979dc9e75f9375df
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2023-08-26 16:46:34 +0200

    2022-9-first-fix

--- 2022/9/main.py
+++ 2022/9/main.py
@@ -1,43 +1,90 @@
-PLUS = 1
-MINUS = 2
-
-def get_turns(path):
+def get_lines(path):
     with open(path) as f:
         return f.read().splitlines()
+    
+def get_turns(lines):
+    turns = []
+    for line in lines:
+        direction, distance = line.split()
+        turns.append([direction, int(distance)])
+    return turns
 
-def get_new_pos(pos1, pos2, operation):
-    if operation == PLUS:
-        return {"x" : pos1["x"] + pos2["x"], "y" : pos1["y"] + pos2["y"]}
+def move_head(direction, positions):
+    if direction == 'U':
+        positions['head'][1] += 1 
+    elif direction == 'D':
+        positions['head'][1] -= 1
+    elif direction == 'L':
+        positions['head'][0] -= 1
     else:
-        return {"x" : pos1["x"] - pos2["x"], "y" : pos1["y"] - pos2["y"]}
+        positions['head'][0] += 1
 
-def move_head(head_pos, direction, unit):
-    if direction == "L":
-        return get_new_pos(head_pos, {"x" : unit, "y": 0}, MINUS)
-    elif direction == "R":
-        return get_new_pos(head_pos, {"x" : unit, "y" : 0}, PLUS)
-    elif direction == "U":
-        return get_new_pos(head_pos, {"x" : 0, "y" : unit}, MINUS)
+def points_not_touching(positions):
+    head = positions['head']
+    tail = positions['tail']
+    dx = head[0] - tail[0]
+    dy = head[1] - tail[1]
+    
+    if dx == 0 and dy == 0: # same position
+        return False
+    elif dx == 0 and abs(dy) == 1: # above or below each other
+        return False
+    elif abs(dx) == 1 and dy == 0: # next to each other
+        return False
+    elif abs(dx) == 1 and abs(dy) == 1: # diagonal to each other
+        return False
     else:
-        return get_new_pos(head_pos, {"x" : 0, "y": unit}, PLUS)
+        return True
 
-def move_tail(tail_pos, head_pos, direction):
-    if direction == "L":
-        return {"x" : head_pos["x"] + 1, "y" : tail_pos["y"]}
-    elif direction == "R":
-        return get_new_pos(head_pos, {"x" : unit, "y" : 0}, PLUS)
-    elif direction == "U":
-        return get_new_pos(head_pos, {"x" : 0, "y" : unit}, MINUS)
-    else:
-        return get_new_pos(head_pos, {"x" : 0, "y": unit}, PLUS)
+def move_tail(positions):
+    head = positions['head'] # ending position
+    tail = positions['tail'] # starting position
+    dx = head[0] - tail[0]
+    dy = head[1] - tail[1]
 
-turns = get_turns('input.txt')
-head_pos = {"x" : 0, "y" : 0}
-tail_pos = {"x" : 0, "y" : 0}
-tail_visited = {}
+    if  dx < 0 and dy == 0: # head is on the left side from the tail
+        tail[0] -= 1
+    elif dx > 0 and dy == 0: # head is on the right side from the tail
+        tail[0] += 1
+    elif dx == 0 and dy > 0: # head is above the tail
+        tail[1] += 1
+    elif dx == 0 and dy < 0: # head is below the tail
+        tail[1] -= 1
+    elif dx > 0 and dy > 0: # head is diagonally up and to the right of the tail
+        tail[0] += 1
+        tail[1] += 1
+    elif dx > 0 and dy < 0: # head is diagonally down and to the right of the tail
+        tail[0] += 1
+        tail[1] -= 1
+    elif dx < 0 and dy > 0: # head is diagonally up and to the left of the tail
+        tail[0] -= 1
+        tail[1] += 1
+    elif dx < 0 and dy < 0: # head is diagonally down and to the left of the tail
+        tail[0] -= 1
+        tail[1] -= 1
+
+def save_tail_position(positions):
+    positions['visited'].add(tuple(positions['tail']))
+    
+def move(turn, positions):
+    direction, distance = turn
+    for i in range(distance):
+        move_head(direction, positions)
+        if points_not_touching(positions):
+            move_tail(positions)
+            save_tail_position(positions)
+
+    
+lines = get_lines('input.txt')
+turns = get_turns(lines)
+
+positions = {
+    "head" : [0, 0],
+    "tail" : [0, 0],
+    "visited" : set((0, 0))
+}
 
 for turn in turns:
-    direction, unit = turn.split()
-    head_pos = move_head(head_pos, direction, int(unit))
-    tail_pos = move_tail(tail_pos, head_pos)
-    print(f'direction : {direction} unit : {unit} head new position: {head_pos}')
+    move(turn, positions)
+
+print(f'Total visited positions of tail: {len(positions["visited"])}')
